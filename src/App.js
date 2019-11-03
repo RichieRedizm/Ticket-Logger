a53bd29a9433c034ca8b0b3f61ae04e4b775656f
Richie Redizm <richieredizm@gmail.com>
2019-11-03 12:57:24 +0000

    basic setup of redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Fragment, useEffect } from 'react'
+import { Provider } from 'react-redux'
+import store from './store'
 import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.min.js'
 import './App.css'
@@ -17,17 +19,19 @@ const App = () => {
     M.AutoInit()
   })
   return (
-    <Fragment>
-      <SearchBar />
-      <div className='container'>
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   )
 }
 
---
+++ src/store.js
@@ -0,0 +1,17 @@
+//npm i redux react-redux redux-thunk redux-devtools-extension
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers'
+
+const initialState = {}
+
+const middleware = [thunk]
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store
